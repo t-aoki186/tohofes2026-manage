ac9f32dd6d7eb618c4db8aad61823556e90683bf
aoki <84th.tccaoki@gmail.com>
2026-05-06 16:25:25 +0900

    add: storageのお知らせと参加団体用JSONをAPI経由で取得可能に変更

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,23 @@ Route::get('/json/news.json', function () {
     $json = Storage::disk('public')->get('json/news.json');
     return response($json, 200)->header('Content-Type', 'application/json');
 });
+
+Route::get('/json/blog.json', function () {
+    if (!Storage::disk('public')->exists('json/blog.json')) {
+        return response()->json(['message' => 'File not found'], 404);
+    }
+
+    // ファイルを読み込んでJSONとして返す
+    $json = Storage::disk('public')->get('json/blog.json');
+    return response($json, 200)->header('Content-Type', 'application/json');
+});
+
+Route::get('/json/organization.json', function () {
+    if (!Storage::disk('public')->exists('json/organization.json')) {
+        return response()->json(['message' => 'File not found'], 404);
+    }
+
+    // ファイルを読み込んでJSONとして返す
+    $json = Storage::disk('public')->get('json/organization.json');
+    return response($json, 200)->header('Content-Type', 'application/json');
+});
