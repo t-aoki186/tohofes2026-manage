6c596af086815cd8f125e9679f80382435990288
aoki <84th.tccaoki@gmail.com>
2026-05-06 15:56:51 +0900

    update: cssのクラス名の変更とcssの整理

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -1,22 +1,22 @@
 <x-manage-layout>
     <section class="pt-25 w-full mx-auto">
         <div class="min-w-full flex flex-wrap gap-5 justify-center py-5">
-            <div class="clock-setting-btn-item">
+            <div class="dash-top-pagelist">
                 <a href="{{ route('manage.inquiry') }}" class="dash-link cursor-pointer">
                     <i class="fa-solid fa-envelope"></i>お問い合わせ確認
                 </a>
             </div>
-            <div class="clock-setting-btn-item">
+            <div class="dash-top-pagelist">
                 <a href="{{ route('manage.post.organization.index') }}" class="dash-link cursor-pointer">
                     <i class="fa-solid fa-circle-plus"></i>参加団体投稿
                 </a>
             </div>
-            <div class="clock-setting-btn-item">
+            <div class="dash-top-pagelist">
                 <a href="{{ route('manage.post.blog.index') }}" class="dash-link cursor-pointer">
                     <i class="fa-solid fa-circle-plus"></i>ブログ投稿
                 </a>
             </div>
-            <div class="clock-setting-btn-item">
+            <div class="dash-top-pagelist">
                 <a href="{{ route('manage.post.news.index') }}" class="dash-link cursor-pointer">
                     <i class="fa-solid fa-circle-plus"></i>お知らせ投稿
                 </a>
