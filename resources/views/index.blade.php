4c19593b276134132aff915d29ae7d9e317784d4
aoki <84th.tccaoki@gmail.com>
2026-05-10 13:00:39 +0900

    fix: ルートページのリンクを修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
+<section class="mx-auto">
     <p>2026年度桐朋祭用APIサーバー</p>
-    <a href="">桐朋祭ホームページに戻る</a>
-</body>
-
-</html>
+    <br>
+    <a href="https://link.tohofes.jp/i/tf26api/">2026年度桐朋祭ホームページに戻る</a>
+</section>
